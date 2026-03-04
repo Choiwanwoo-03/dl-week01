8baa3e445ea69ff2aff4b9dbb5437668cb48b94f
최완우 <chldhksdn37@gmail.com>
2026-03-04 09:49:47 +0900

    Updated Changes

--- UpDown.py
+++ UpDown.py
@@ -10,7 +10,12 @@ game_count = 1
 while True :
     try :
         my_number = int(input("1 ~ 100 사이의 숫자를 입력하세요"))
+        if my_number > random_number :
+            print("Down")
+        elif my_number < random_number :
+            print("Up")
     except :
+        print("collect")
 
 
 
